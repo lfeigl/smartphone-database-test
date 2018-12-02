841b12f7c71cac3ef61c577e1067c658a3094728
Luca Feigl <sarius009@gmail.com>
2018-12-02 15:54:57 +0100

    Add internal id property

--- SmartphoneApp/Own.SmartphoneLib/Smartphone.cs
+++ SmartphoneApp/Own.SmartphoneLib/Smartphone.cs
@@ -8,6 +8,7 @@ namespace Own.SmartphoneLib
 {
     public class Smartphone
     {
+        public int InternalId { get; set; }
         public string Manufacturer { get; set; }
         public string Model { get; set; }
         public double Price { get; set; }
