538a60c5801f3ce3baf40137207b467e8c6bdb75
Luca Feigl <sarius009@gmail.com>
2018-12-03 22:43:31 +0100

    Add domain object class

---
+++ SmartphoneApp/Own.SmartphoneLib/DomainObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Own.SmartphoneLib
+{
+    public abstract class DomainObject
+    {
+        public Guid Id { get; set; }
+
+        public DomainObject()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
