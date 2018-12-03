9e55c313c531586a8dc341511cc9ce31941286f9
Luca Feigl <sarius009@gmail.com>
2018-12-03 20:36:36 +0100

    Add sample smartphones

--- SmartphoneApp/Own.SmartphoneTest/Program.cs
+++ SmartphoneApp/Own.SmartphoneTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Own.SmartphoneLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace Own.SmartphoneTest
     {
         static void Main(string[] args)
         {
+            SmartphoneList spList = new SmartphoneList();
+            Smartphone sp = new Smartphone();
+
+            sp.InternalId = 1;
+            sp.Manufacturer = "Apple";
+            sp.Model = "iPhone X";
+            sp.Price = 850.99;
+
+            spList.Add(sp);
+
+
+            sp = new Smartphone();
+
+            sp.InternalId = 2;
+            sp.Manufacturer = "Samsung";
+            sp.Model = "Galaxy S9";
+            sp.Price = 500.99;
+
+            spList.Add(sp);
+
+
+            foreach (Smartphone listSp in spList)
+            {
+                Console.WriteLine("Internal ID: " + listSp.InternalId);
+                Console.WriteLine("Manufacturer: " + listSp.Manufacturer);
+                Console.WriteLine("Model: " + listSp.Model);
+                Console.WriteLine("Price: " + listSp.Price);
+                Console.WriteLine("- - -");
+            }
         }
     }
 }
