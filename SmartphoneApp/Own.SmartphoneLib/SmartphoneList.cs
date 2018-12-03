d5cc0415c35a76f70fe8783bd4ef7d3da03e814f
Luca Feigl <sarius009@gmail.com>
2018-12-03 21:05:58 +0100

    Add sample methods

--- SmartphoneApp/Own.SmartphoneLib/SmartphoneList.cs
+++ SmartphoneApp/Own.SmartphoneLib/SmartphoneList.cs
@@ -8,5 +8,45 @@ namespace Own.SmartphoneLib
 {
     public class SmartphoneList : List<Smartphone>
     {
+        public Smartphone GetByInternalId(int internalId)
+        {
+            Smartphone sp = null;
+
+            foreach (Smartphone listSp in this)
+            {
+                if (listSp.InternalId == internalId)
+                {
+                    sp = listSp;
+                    break;
+                }
+            }
+
+            return sp;
+        }
+
+        public Smartphone GetCheapest()
+        {
+            Smartphone sp = this[0];
+
+            foreach (Smartphone listSp in this)
+            {
+                if (listSp.Price < sp.Price)
+                {
+                    sp = listSp;
+                }
+            }
+
+            return sp;
+        }
+
+        public void SetDiscount(int percentage)
+        {
+            double discount = (double)percentage / 100;
+
+            foreach (Smartphone listSp in this)
+            {
+                listSp.Price -= Math.Round(listSp.Price * discount, 2);
+            }
+        }
     }
 }
